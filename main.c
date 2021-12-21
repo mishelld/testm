2393ca1c4cf778ccff55ca72ff4d64b89ea97038
mishelld <93199677+mishelld@users.noreply.github.com>
2021-12-21 13:02:03 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -8,8 +8,6 @@
 const int abc[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
                      's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-
-
 void insertWord(char word[WORD]){
     int count = 0;
     char input;
